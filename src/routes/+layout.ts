3204d78bb95d1e6c984618e1604fb6b3afd158ab
ORANGETAN3422 <orangtan3422@gmail.com>
2026-03-22 16:53:53 +1100

    setup pages

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,8 @@ import type { Flags } from '$lib/types';
 
 const flags = flagsData as unknown as Flags;
 
+export const prerender = true;
+
 export async function load() {
   const pages = Object.keys(flags).map(key => ({
     slug: encodeURIComponent(key),
